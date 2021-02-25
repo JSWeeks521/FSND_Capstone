b5995bf326fd391a802b879966e716d83e5edb8f
Jason Weeks <67436181+JSWeeks521@users.noreply.github.com>
2021-02-25 14:06:36 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, Date, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_path = os.environ['DATABASE_URL']
+database_path = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy()
 
